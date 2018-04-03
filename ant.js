45c71b14f9416c92d13ddae1b4f9b2556ce6d3b3
Peter Bae <pbae@uw.edu>
2018-04-02 18:30:08 -0700

    pheromones

--- ant.js
+++ ant.js
@@ -1,19 +1,10 @@
 function Ant(game, xPos, yPos) {
+	this.xPos = xPos;
+	this.yPos = yPos;
 	this.game = game;
 	this.ctx = game.ctx;
-	this.neighbors = [];	
+	this.tiles = [];	
 	var rand = Math.random();
-	this.level = 0;
-	if (rand < 0.6) {
-		this.level = 0;
-	} else if (rand < 0.7) {
-		this.level = 1;
-	} else if (rand < 0.8) {
-		this.level = 2;
-	} else {
-		this.level = 3;
-	}
-	//this.alive = Math.random() > .90 ? true : false;
 	Entity.call(this, game, xPos * 10, yPos * 10);
 }
 
@@ -21,49 +12,18 @@ Ant.prototype = new Entity();
 Ant.prototype.constructor = Ant;
 
 Ant.prototype.update = function() {
-	var neighborLevels = 0;
-	
-	for (var i = 0; i < 8; i++) {
-		neighborLevels += this.neighbors[i].level;
-	}
-	
-	if (this.level === 0 && neighborLevels >= 2) {
-		this.level = 1;
-	} else if (this.level === 1 && neighborLevels <= 4) {
-		this.level = 0;
-	} else if (this.level === 1 && neighborLevels >= 8 && neighborLevels <= 15) {
-		this.level = 2;
-	} else if (this.level === 2 && (neighborLevels < 10 || neighborLevels > 16)) {
-		this.level = 0;
-	} else if (this.level === 2 && neighborLevels >= 10 && neighborLevels <= 12) {
-		this.level = 1;
-	} else if (this.level === 2 && neighborLevels >= 14 && neighborLevels <= 16) {
-		this.level = 3;
-	} else if (this.level === 3 && (neighborLevels < 15 || neighborLevels > 21)) {
-		this.level = 0;
-	} else if (this.level === 3 && neighborLevels >= 15 && neighborLevels <= 18) {
-		this.level = 1;
-	} else if (this.level === 3 && neighborLevels >= 19 && neighborLevels <= 21) {
-		this.level = 2;
-	}
+	var rand = Math.random();
 	
 	this.draw();
 }
 
 Ant.prototype.draw = function() {
+	this.ctx.fillStyle = "#ABCDEF";
+	
+	this.ctx.fillRect(this.x+2, this.y+2, 6, 6);
+	
 	this.ctx.fillStyle = "black";
 	this.ctx.strokeRect(this.x, this.y, 10, 10);
-	if (this.level === 3) {
-		this.ctx.fillStyle = "#000000";
-	} else if (this.level === 2) {
-		this.ctx.fillStyle = "#555555";
-	} else if (this.level === 1) {
-		this.ctx.fillStyle = "#AAAAAA";
-	}else {
-		this.ctx.fillStyle = "#FFFFFF";
-	}
-	
-	this.ctx.fillRect(this.x, this.y, 10, 10);
 }
 
 Ant.prototype.setNeighbors = function(neighbors) {
--- tile.js
+++ tile.js
@@ -1,12 +1,14 @@
 function Tile(game, xPos, yPos, isHome) {
+	var rand = Math.random();
 	this.game = game;
 	this.ctx = game.ctx;
-	this.outgoingPheromoneLevel = 0;
-	this.incomingPheromoneLevel = 0;
+	this.outgoingPheromoneLevel = rand > .8 ? 7 : 0;
+	rand = Math.random();
+	this.incomingPheromoneLevel = rand > .8 ? 4 : 0;
 	this.foodLevel = 0;
 	this.isHome = isHome;
 	this.neighbors = [];	
-	var rand = Math.random();
+	
 	if (rand < 0.95) {
 		this.foodLevel = 0;
 	} else {
@@ -19,25 +21,109 @@ Tile.prototype = new Entity();
 Tile.prototype.constructor = Tile;
 
 Tile.prototype.update = function() {
+	var maxOut = this.outgoingPheromoneLevel;
+	var maxIn = this.incomingPheromoneLevel;
+	for (var i = 0; i < 4; i++) {
+		if (this.neighbors[i].outgoingPheromoneLevel-1 > maxOut)
+			maxOut = this.neighbors[i].outgoingPheromoneLevel-1;
+		if (this.neighbors[i].incomingPheromoneLevel-1 > maxIn)
+			maxIn = this.neighbors[i].incomingPheromoneLevel-1;
+	}
+	
+	this.outgoingPheromoneLevel = 
+		this.outgoingPheromoneLevel < maxOut ?
+		maxOut : this.outgoingPheromoneLevel;
+	this.incomingPheromoneLevel = 
+		this.incomingPheromoneLevel < maxIn ?
+		maxIn : this.incomingPheromoneLevel;
 	this.draw();
 }
 
 Tile.prototype.draw = function() {
-	this.ctx.fillStyle = "black";
-	this.ctx.strokeRect(this.x, this.y, 10, 10);
-	if (this.foodLevel > 0) {
-		this.ctx.fillStyle = "#DD3333";
-	} else {
-		this.ctx.fillStyle = "#FFFFFF";
-	}
-	this.ctx.fillRect(this.x, this.y, 5, 5);
-	
 	if (this.isHome) {
 		this.ctx.fillStyle = "#113355";
 		this.ctx.fillRect(this.x, this.y, 10, 10);
+	} else {
+		if (this.foodLevel > 0) {
+			this.ctx.fillStyle = "#000000";
+		} else {
+			this.ctx.fillStyle = "#FFFFFF";
+		}
+		this.ctx.fillRect(this.x, this.y, 5, 5);
+		
+		switch (this.outgoingPheromoneLevel) {
+			case 0:
+				this.ctx.fillStyle = "#FFFFFF";
+				break;
+			case 1:
+				this.ctx.fillStyle = "#FFE6E6";
+				break;
+			case 2:
+				this.ctx.fillStyle = "#FFCDCD";
+				break;
+			case 3:
+				this.ctx.fillStyle = "#FFB4B4";
+				break;
+			case 4:
+				this.ctx.fillStyle = "#FF9B9B";
+				break;
+			case 5:
+				this.ctx.fillStyle = "#FF8282";
+				break;
+			case 6:
+				this.ctx.fillStyle = "#FF6969";
+				break;
+			case 7:
+				this.ctx.fillStyle = "#FF5050";
+				break;
+			case 8:
+				this.ctx.fillStyle = "#FF3737";
+				break;
+			case 9:
+				this.ctx.fillStyle = "#FF1E1E";
+				break;
+			case 10:
+				this.ctx.fillStyle = "#FF0000";
+		}
+		this.ctx.fillRect(this.x+5, this.y, 5, 5);
+		switch (this.incomingPheromoneLevel) {
+			case 0:
+				this.ctx.fillStyle = "#FFFFFF";
+				break;
+			case 1:
+				this.ctx.fillStyle = "#E6E6FF";
+				break;
+			case 2:
+				this.ctx.fillStyle = "#CDCDFF";
+				break;
+			case 3:
+				this.ctx.fillStyle = "#B4B4FF";
+				break;
+			case 4:
+				this.ctx.fillStyle = "#9B9BFF";
+				break;
+			case 5:
+				this.ctx.fillStyle = "#8282FF";
+				break;
+			case 6:
+				this.ctx.fillStyle = "#6969FF";
+				break;
+			case 7:
+				this.ctx.fillStyle = "#5050FF";
+				break;
+			case 8:
+				this.ctx.fillStyle = "#3737FF";
+				break;
+			case 9:
+				this.ctx.fillStyle = "#1E1EFF";
+				break;
+			case 10:
+				this.ctx.fillStyle = "#0000FF";
+		}
+		this.ctx.fillRect(this.x+5, this.y+5, 5, 5);
 	}
-	
-	
+	this.ctx.fillStyle = "black";
+	this.ctx.strokeRect(this.x, this.y, 10, 10);	
 }
 
 Tile.prototype.setNeighbors = function(neighbors) {
