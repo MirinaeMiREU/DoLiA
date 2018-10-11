d893db420828f86d6dd555b084383714f2c7fa57
Peter Bae <pbae@uw.edu>
2018-10-11 14:09:40 -0700

    testing new library

---
+++ data/graph.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import LogNorm
+
+Z = np.random.rand(6, 10)
+
+fig, (ax0, ax1) = plt.subplots(2, 1)
+
+c = ax0.pcolor(Z)
+ax0.set_title('default: no edges')
+
+c = ax1.pcolor(Z, edgecolors='k', linewidths=4)
+ax1.set_title('thick edges')
+
+fig.tight_layout()
+plt.show()
