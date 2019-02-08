e6e79fb8274e421b00ccd4bad9d2cf51e51088d3
Peter Bae <pbae@uw.edu>
2019-02-07 23:33:35 -0800

    data collection version 2

--- showGraphs.js
+++ showGraphs.js
@@ -1,5 +1,10 @@
-
-socket.emit("loadAnts", {mode: "test"});
+var socket = io.connect("http://24.16.255.56:8888");
+socket.on("connect", function() {
+    console.log("connected on output");
+});
+/*
+socket.emit("loadAnts", {"params.run": "Run#1"});
 socket.on("loadAnts", function(e) {
     console.log(e);
 });
+*/
