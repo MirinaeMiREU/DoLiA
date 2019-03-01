57aca71056cc922080c56158ea817b12ffe84f93
Peter Bae <pbae@uw.edu>
2019-03-01 02:16:43 -0800

    Update gameengine.js

--- gameengine.js
+++ gameengine.js
@@ -269,8 +269,8 @@ GameEngine.prototype.setSettings = function() {
 			breedSpeed: true, 
 			foodCarry: true, 
 			energy: true,
-			fWeight: 1,
-			bWeight: 2
+			fWeight: 2,
+			bWeight: 3
 		});
 	}
 /*
